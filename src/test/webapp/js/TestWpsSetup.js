391ee76b6757f9973b81f1d662ad2cb0bf41d485
matthesrieke <m.rieke@52north.org>
2013-06-24 14:27:52 +0200

    add javascript testing

---
+++ src/test/webapp/js/TestWpsSetup.js
@@ -0,0 +1,13 @@
+TestCase('TestWpsSetup', {
+
+    testShouldDefineProxy : function() {
+		jQuery.wpsSetup({
+			proxy : {
+				url : "http://localhost:8080/jprox/parameter?targetUrl=",
+				type : "parameter"
+			}
+		});
+        assertEquals('The PROXY_URL was not as expected.', 'http://localhost:8080/jprox/parameter?targetUrl=', PROXY_URL);
+    }
+
+});
