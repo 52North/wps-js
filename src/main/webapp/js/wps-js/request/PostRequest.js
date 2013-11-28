559a592a96908e2627f8557e52afd61dc29af660
matthesrieke <m.rieke@52north.org>
2013-11-28 16:26:49 +0100

    add a XML templating function + abstract method for payload creation

--- src/main/webapp/js/wps-js/request/PostRequest.js
+++ src/main/webapp/js/wps-js/request/PostRequest.js
@@ -4,8 +4,25 @@ var PostRequest = BaseRequest.extend({
 	prepareHTTPRequest : function() {
 		return {
 			type : "POST",
-			data : this.settings.data
+			data : this.createPostPayload(),
+			contentType : "text/xml"
 		};
+	},
+	
+	createPostPayload : function() {
+		return null;
+	},
+	
+	fillTemplate : function(template, properties) {
+		var result = template;
+		
+		for (var key in properties) {
+			if (properties.hasOwnProperty(key)) {
+				result = result.replace("${"+key+"}", properties[key]);	
+			}
+		}
+		
+		return result;
 	}
 
 });
